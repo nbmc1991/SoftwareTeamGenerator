a5fd97628b02ee9ecc9eaa70f5195899a1c0a756
nbmc1991 <nbmc1991@gmail.com>
2020-11-17 17:42:10 -0500

    passed employee test

--- develop/lib/Employee.js
+++ develop/lib/Employee.js
@@ -1,1 +1,56 @@
 // TODO: Write code to define and export the Employee class
+class Employee{
+constructor (name, id, email,) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    
+    // this.getName = () => console.log(`Name: ${this.name}`);
+    // //method which get the ID
+    // this.getId = () => console.log(`ID: ${this.id}`);
+    // //method which get the EMAIL
+    // this.getEmail = () => console.log(`Email: ${this.email}`);
+    // //method displays ROLE in this case is EMPLOYEE ROLE
+    // this.getRole = () => console.log(`Role: Employee`);
+}
+
+getName () {
+    return this.name;
+}
+
+getId(){
+    return this.id;
+}
+
+getEmail() {
+    return this.email;
+}
+
+getRole() {
+    return 'Employee';
+}
+
+
+
+
+
+
+}
+
+const newEmp = new Employee('norma', 911,);
+newEmp.getName();
+newEmp.getId();
+newEmp.getEmail();
+newEmp.getRole();
+
+
+module.exports = Employee;
+
+
+// const employee = new Employee("norma", e911pv, nbmc1991);
+// console.log(employee)
+
+// employee.getName();
+// employee.getId();
+// employee.getEmail();
+
--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require ('./Employee');
+
+class Enginner extends Employee {
+    constructor (username){
+        super(norma, 911, 'nbmc1991');
+        this.username = username ;
+    }
+
+    getGithub(){
+        return this.username;
+    }
+
+    getRole(){
+        return 'Engineer';
+    }
+
+
+}
+getGithub();
+
